a3427e1678f4d93cf65d0082a165e26fe0e15f1f
dustss <oneisoneonlyone@foxmail.com>
2018-04-22 17:59:46 +0530

    network data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,28 @@
 // pages/index/index.js
 
 const blockCN = [
-  '国内', '国际', '财经', '娱乐', '技术', '体育', '其他'
+  '国内', '国际', '财经', '娱乐', '军事', '体育', '其他'
 ]
+
 const blockCNs = [
   'gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'
 ]
 
 
 Page({
-    
+
   /**
    * 页面的初始数据
    */
   data: {
-    blockData:[]
+    blockData: [],
+    index: 0
   },
 
-  setBlockData (){
+  setBlockData() {
     let blockData = []
     for (let i = 0; i < 7; i++) {
       blockData.push({
-        index:i ,
         blockName: blockCN[i]
       })
     }
@@ -29,9 +30,10 @@ Page({
   },
 
   /*点击板块名称 显示对应板块的列表 */
-  onClickListener(){
+  onClickListener(e) {
+    console.log(e.target.id)
     wx.navigateTo({
-      url: '/pages/sub/sub?' + '国内'
+      url: '/pages/sub/sub?type=' + blockCNs[e.target.id]
     })
   },
 
@@ -47,48 +49,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
--- pages/sub/sub.js
+++ pages/sub/sub.js
@@ -5,68 +5,74 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    subBlockNews: [],
+    type: 'gj'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    // this.subBlockNews.push({
+    //   id: '12345678',
+    //   newsTitle: '亚洲媒体高峰会议来了！',
+    //   newsTime: '2018-04-06T11:28:25.000Z',
+    //   newsFrome: '环球资讯',
+    //   newsPicturePath: 'http://inews.gtimg.com/newsapp_bt/0/3199649303/641'     //初始化数据
+    // })
+    this.setData({
+      type: options.type
+    })
+    this.getSubBlockNews()
   },
-/*点击新闻条目显示详情页面，传入参数 id */
-  onClickListener(){
-      wx.navigateTo({
-        url: '/pages/detail/detail?'+'id=xxxx'
+  /*点击新闻条目显示详情页面，传入参数 id */
+  onClickListener() {
+    wx.navigateTo({
+      url: '/pages/detail/detail?' + 'id=xxxx'
+    })
+  },
+
+  /**
+   * 从网络侧获取对应板块的新闻列表数据
+   */
+  getSubBlockNews(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.type
+      },
+      success: res => {
+        this.setSubBlockNews(res.data.result)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+  /**
+   * 设置子版块新闻列表内容
+   */
+  setSubBlockNews(result) {
+    let subBlockNews = []
+    for (let i = 0; i < result.length ; i++) {
+      subBlockNews.push({
+        id:result[i].id,
+        newsTitle: result[i].title,
+        newsTime: result[i].date,
+        newsFrome: result[i].source,
+        newsPicturePath:result[i].firstImage              //需要考虑到没有图片的情况
       })
+    }
+    this.setData({
+      subBlockNews: subBlockNews
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  /*下拉列表刷新内容 */
+  onPullDownRefresh() {
+    this.getSubBlockNews(() => {
+      wx.stopPullDownRefresh()
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
