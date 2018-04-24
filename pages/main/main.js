81de22c801b9cabb987b3e711db1e320dc44980c
dustss <oneisoneonlyone@foxmail.com>
2018-04-25 00:03:32 +0530

    下拉刷新待修复

--- pages/main/main.js
+++ pages/main/main.js
@@ -1,66 +1,103 @@
-// pages/main/main.js
+
+const blockCN = [
+  '国内', '国际', '财经', '娱乐', '军事', '体育', '其他'
+]
+
+const blockCNs = [
+  'gn', 'gj', 'cj', 'yl', 'js', 'ty', 'other'
+]
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    blockData: [],
+    selectedID: 1,
+    subBlockNews: [],
+    "type": 'gj'
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setBlockData()
+    this.getSubBlockNews(this.data.selectedID)
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 填充tab的板块名称
    */
-  onReady: function () {
-  
+  setBlockData() {
+    let blockData = []
+    for (let i = 0; i < blockCN.length; i++) {
+      blockData.push({
+        blockName: blockCN[i]
+      })
+    }
+    this.setData({ blockData: blockData })
+  },
+  /**
+   * 点击tab 选择相应板块 
+   */
+  onBlockSelect(e) {
+    var ID = e.currentTarget.id
+    if (this.selectedID === ID)
+    { return false }
+    this.setData({
+      selectedID: ID
+    })
+    this.getSubBlockNews(ID)
   },
 
-  /**
-   * 生命周期函数--监听页面显示
+    /**
+   * 从网络侧获取对应板块的新闻列表数据
    */
-  onShow: function () {
-  
+  getSubBlockNews(id) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        "type": blockCNs[id]
+      },
+      success: res => {
+        this.setSubBlockNews(res.data.result)
+      },
+    })
+  },
+  /**
+   * 设置子版块新闻列表内容
+   */
+  setSubBlockNews(result) {
+    let subBlockNews = []
+    for (let i = 0; i < result.length; i++) {
+      subBlockNews.push({
+        newsID: result[i].id || "12345678",
+        newsTitle: result[i].title || "标题加载中...",
+        newsTime: (result[i].date || "2008-08-08").substr(0, 10),
+        newsFrom: result[i].source,
+        newsPicturePath: result[i].firstImage || '/images/logo_black_trans.png'     //空值处理
+      })
+      this.setData({
+        subBlockNews: subBlockNews
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  /*下拉列表刷新内容 */
+  onPullDownRefresh() {
+    this.getSubBlockNews(this.data.selectedID)
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  /*点击新闻条目显示详情页面，传入参数 id */
+  onFurtherInfo(e) {
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + this.data.subBlockNews[e.currentTarget.id].newsID
+    })
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
